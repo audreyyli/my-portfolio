fdb8aeba0e3a3a3d1a86f74ce454f558739af5a9
audreyyli <li.audrey04@gmail.com>
2024-10-01 20:52:22 -0400

    fixing errors

--- my-portfolio/next.config.js
+++ my-portfolio/next.config.js
@@ -1,9 +1,5 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  assetPrefix: isProd ? '/my-portfolio/' : '',
-  images: {
-    unoptimized: true,
-  },
-  trailingSlash: true,
+  basePath: "/my-portfolio",
 };
